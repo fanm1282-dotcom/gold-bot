f8e6afc5295c943daf7b0b054c78a3c7390e3271
fanm1282-dotcom <fanm1282@gmail.com>
2026-05-28 15:15:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const {
   Client,
-  GatewayIntentBits
+  GatewayIntentBits,
+  Events
 } = require('discord.js');
 
 const client =
@@ -25,6 +26,95 @@ client.once('ready', () => {
   );
 
 });
+
+/* =========================
+   INTERACTION
+========================= */
+
+client.on(
+
+  Events.InteractionCreate,
+
+  async interaction => {
+
+    if(
+      !interaction.isChatInputCommand()
+    ) return;
+
+    /* =====================
+       PING
+    ===================== */
+
+    if(
+      interaction.commandName ===
+      'ping'
+    ){
+
+      await interaction.reply(
+        'pong 🏓'
+      );
+
+    }
+
+    /* =====================
+       STATUS
+    ===================== */
+
+    if(
+      interaction.commandName ===
+      'status'
+    ){
+
+      await interaction.reply({
+
+        content:
+        '🟢 ระบบออนไลน์',
+
+        ephemeral:true
+
+      });
+
+    }
+
+    /* =====================
+       BUY
+    ===================== */
+
+    if(
+      interaction.commandName ===
+      'buy'
+    ){
+
+      await interaction.reply({
+
+        content:
+        '🟢 เปิด Buy XAUUSD สำเร็จ'
+
+      });
+
+    }
+
+    /* =====================
+       SELL
+    ===================== */
+
+    if(
+      interaction.commandName ===
+      'sell'
+    ){
+
+      await interaction.reply({
+
+        content:
+        '🔴 เปิด Sell XAUUSD สำเร็จ'
+
+      });
+
+    }
+
+  }
+
+);
 
 /* =========================
    LOGIN
